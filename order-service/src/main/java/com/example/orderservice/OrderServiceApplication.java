6f3be8c70e6db372e158d5e838c323a0c72759b5
Srikar2610 <srikarpadakanti10@gmail.com>
2025-06-02 14:38:34 +0530

    Update OrderServiceApplication.java

--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -1,1 +1,19 @@
+package com.example.orderservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return "Hello from Order Service";
+    }
+}
