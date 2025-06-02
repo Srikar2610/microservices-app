e5b333ceea3d227bd236d161c9a466dfea3f5683
Srikar2610 <srikarpadakanti10@gmail.com>
2025-06-02 14:36:41 +0530

    Create UserServiceApplication.java

---
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return "Hello from User Service";
+    }
+}
